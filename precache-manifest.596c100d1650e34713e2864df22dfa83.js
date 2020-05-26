4bf75d7fe031c67983a11639b14635f4b73e6b42
jOh <j0h@example.com>
2020-05-25 22:47:06 -0300

    Updates

---
+++ precache-manifest.596c100d1650e34713e2864df22dfa83.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8384216be9c20f01b459ae900c22447a",
+    "url": "/adrianmarceloledesma/App-Control-de-Gastos/index.html"
+  },
+  {
+    "revision": "756354c6078f8ef7c7dc",
+    "url": "/adrianmarceloledesma/App-Control-de-Gastos/static/css/main.4b018416.chunk.css"
+  },
+  {
+    "revision": "075e1184b57b89ee0aa4",
+    "url": "/adrianmarceloledesma/App-Control-de-Gastos/static/js/2.07b9bee2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/adrianmarceloledesma/App-Control-de-Gastos/static/js/2.07b9bee2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "756354c6078f8ef7c7dc",
+    "url": "/adrianmarceloledesma/App-Control-de-Gastos/static/js/main.ba8aa435.chunk.js"
+  },
+  {
+    "revision": "b0f14575c82e0aa52d4a",
+    "url": "/adrianmarceloledesma/App-Control-de-Gastos/static/js/runtime-main.75118163.js"
+  }
+]);
